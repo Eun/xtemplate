da25e4ed2820063ce008da91f8db4a94edded9b6
Tobias Salzmann <796084+Eun@users.noreply.github.com>
2025-09-15 15:06:56 +0200

    fix: allow returning any values (#4)
    * allow returning different types than string
    
    * fix param

--- fn_tmpl.go
+++ fn_tmpl.go
@@ -6,6 +6,13 @@ import (
 	"fmt"
 )
 
+// OnlyOneArgumentIsAllowedError indicates that only one argument is allowed.
+type OnlyOneArgumentIsAllowedError struct{}
+
+func (e OnlyOneArgumentIsAllowedError) Error() string {
+	return "only one argument is allowed"
+}
+
 // Tmpl provides enhanced template execution capabilities.
 type Tmpl rootContext
 
@@ -16,13 +23,19 @@ type Tmpl rootContext
 //	{{ define "T1" }}Hello {{ . }}{{ end }}
 //	{{ $result := tmpl.Exec "T1" "World" }}
 //	Message: {{ $result }} // Output: Message: Hello World
-func (ctx Tmpl) Exec(name string, data any) (string, error) {
+func (ctx Tmpl) Exec(name string, data ...any) (any, error) {
+	var arg any
 	var buf bytes.Buffer
-	err := ctx.template.ExecuteTemplate(&buf, name, data)
+	if len(data) > 1 {
+		return nil, OnlyOneArgumentIsAllowedError{}
+	} else if len(data) == 1 {
+		arg = data[0]
+	}
+	err := ctx.template.ExecuteTemplate(&buf, name, arg)
 	if err != nil {
 		var retErr ReturnError
 		if errors.As(err, &retErr) {
-			return fmt.Sprint(retErr.Value), nil
+			return retErr.Value, nil
 		}
 		return "", fmt.Errorf("failed to execute partial template %q: %w", name, err)
 	}
