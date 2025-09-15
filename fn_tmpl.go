099f2ff5bfa4a7fdd095c0ecc0fef1d4a4101d12
Tobias Salzmann <796084+Eun@users.noreply.github.com>
2025-09-15 15:04:03 +0200

    fix param

--- fn_tmpl.go
+++ fn_tmpl.go
@@ -24,13 +24,14 @@ type Tmpl rootContext
 //	{{ $result := tmpl.Exec "T1" "World" }}
 //	Message: {{ $result }} // Output: Message: Hello World
 func (ctx Tmpl) Exec(name string, data ...any) (any, error) {
+	var arg any
 	var buf bytes.Buffer
 	if len(data) > 1 {
 		return nil, OnlyOneArgumentIsAllowedError{}
-	} else if len(data) == 0 {
-		data = nil
+	} else if len(data) == 1 {
+		arg = data[0]
 	}
-	err := ctx.template.ExecuteTemplate(&buf, name, data)
+	err := ctx.template.ExecuteTemplate(&buf, name, arg)
 	if err != nil {
 		var retErr ReturnError
 		if errors.As(err, &retErr) {
